5f4862c206a2c419dbb0a4565943ea316b803cb8
Don McCaughey <don@donm.cc>
2016-06-21 12:22:37 -0700

    In macOS app Document class, manage an mg_graph struct.
    Create a new mg_graph on creation, implement dataOfType() for writing
    and readFromData() for reading mg_graph struct from a file.

---
+++ MonographApp/Errors.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+public let MonographAppErrorDomain = "MonographApp"
+
+
+public enum MonographAppErrorCode: Int {
+    case UnrecognizedDocumentType = 1
+}
--- MonographApp_macos/Document.swift
+++ MonographApp_macos/Document.swift
@@ -7,14 +7,22 @@
 //
 
 import Cocoa
+import Darwin.C.errno
+import monograph
+
 
 class Document: NSDocument {
-
+    var graph: UnsafeMutablePointer<mg_graph>
+    
     override init() {
+        graph = mg_graph_alloc()
         super.init()
-        // Add your subclass-specific initialization here.
     }
-
+    
+    deinit {
+        mg_graph_free(graph)
+    }
+    
     override class func autosavesInPlace() -> Bool {
         return true
     }
@@ -27,18 +35,28 @@ class Document: NSDocument {
     }
 
     override func dataOfType(typeName: String) throws -> NSData {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        var length: Int32 = 0
+        let string = mg_graph_alloc_string(graph, &length)
+        if (string == nil) {
+            throw NSError(domain: NSOSStatusErrorDomain, code: Int(errno), userInfo: nil)
+        }
+        return NSData(bytesNoCopy: string, length: Int(length), freeWhenDone: true)
     }
 
     override func readFromData(data: NSData, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        NSLog("INFO: typeName = \"\(typeName)\"")
+        var length: Int32 = 0
+        let string = UnsafePointer<Int8>(data.bytes)
+        let new_graph = mg_graph_alloc_from_string(string, &length)
+        if (new_graph == nil) {
+            throw NSError(domain: NSOSStatusErrorDomain, code: Int(errno), userInfo: nil)
+        }
+        if Int(length) < data.length {
+            // TODO: should we check unread bytes at end of file, or
+            // should libmonograph do this?
+            NSLog("WARNING: did not read \(data.length - Int(length)) bytes of file")
+        }
+        mg_graph_free(graph)
+        graph = new_graph
     }
-
-
 }
-
